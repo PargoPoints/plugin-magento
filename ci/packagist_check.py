630699f24f2e68fecfeb61ac18090c289f8f927d
devinsmith911 <devin.smith@pargo.co.za>
2022-12-15 14:18:09 +0200

    chore: cleanup

--- ci/packagist_check.py
+++ ci/packagist_check.py
@@ -9,29 +9,6 @@ GIT_BRANCH_REF = sys.argv[2]
 REPO_API_URL = 'https://packagist.org/packages/PargoPoints/plugin-magento.json'
 
 def get_package_version():
-  print ("Git Ref: {}".format(GIT_BRANCH_REF))
-  #version_data = requests.get(REPO_API_URL)
-  headers = {'Cache-Control': 'no-cache', 'Pragma': 'no-cache', 'Expires': 'Thu, 01 Jan 1970 00:00:00 GMT'}
-  source_ref = 'a'
-  dist_ref = 'b'
-  while dist_ref != GIT_BRANCH_REF and source_ref != GIT_BRANCH_REF:
-    try:
-        version_data = http.request('GET', REPO_API_URL, headers=headers)
-        version_json = (json.loads(version_data.data))
-        print (json.dumps(version_json,indent=4))
-        print ("Packagist Source Ref: {}, Packagist Dist Ref: {}".format(source_ref,dist_ref))
-        source_ref = (version_json['package']['versions'][BRANCH_NAME]['source']['reference'])
-        dist_ref = (version_json['package']['versions'][BRANCH_NAME]['dist']['reference'])
-        print ("Checking branch refs...")
-    except Exception as e:
-        print ("Unable to find packagist info for {}".format(e))
-    time.sleep(5)
-
-  print ("GIT_REF matches packagist refs")
-
-#get_package_version()
-
-def test():
   REPO_API_URL = 'https://repo.packagist.org/p2/pargopoints/plugin-magento~dev.json'
   headers = {'Cache-Control': 'no-cache', 'Pragma': 'no-cache', 'Expires': 'Thu, 01 Jan 1970 00:00:00 GMT'}
   source_ref = 'a'
@@ -39,6 +16,7 @@ def test():
   while dist_ref != GIT_BRANCH_REF and source_ref != GIT_BRANCH_REF:
     version_data = http.request('GET', REPO_API_URL, headers=headers)
     version_json = (json.loads(version_data.data))
+    print (json.dumps(version_json, indent=4))
     for item in version_json['packages']['pargopoints/plugin-magento']:
       if item['version'] == BRANCH_NAME:
         source_ref = (item['source']['reference'])
@@ -46,4 +24,4 @@ def test():
     print (source_ref)
     print (dist_ref)
 
-test()
+get_package_version()
